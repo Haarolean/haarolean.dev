637bcdad10d03c61ee91252760323a79c84bf680
Roman Zabaluev <gpg@haarolean.dev>
2025-05-06 13:42:02 +0700

    Fix content sizes on mobile

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
             <NyanBackground />
             <div className="flex h-screen min-h-screen flex-col">
                 {/*<NavBar />*/}
-                <main className="mx-auto flex h-full w-full flex-1 items-center justify-center">
+                <main className="mx-auto flex w-full flex-1 items-center justify-center overflow-hidden">
                     <div className="flex w-full p-8 lg:mx-96 lg:p-0">
                         <div className="w-96 flex-1">
                             <h1 className="font-heading text-4xl font-bold leading-relaxed lg:text-5xl ">
@@ -36,7 +36,7 @@ export default function Index() {
                         </div>
                     </div>
                 </main>
-                <div className="flex-2 flex items-center justify-center">
+                <div className="flex flex-none items-center justify-center">
                     <Footer />
                 </div>
             </div>
